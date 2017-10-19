d23f2a72fa67f4482c1112f1c54faa92c7320836
Andreas Untergasser <andreas@untergasser.de>
2017-10-18 23:45:43 -0700

    Fixed Position Calculation

--- src/silica.cpp
+++ src/silica.cpp
@@ -395,17 +395,17 @@ int main(int argc, char** argv) {
 	      PrimerBind prim;
 	      prim.refIndex = refIndex;
 	      prim.temp = o.temp;
-              prim.perfTemp = matchTemp;
+	      prim.perfTemp = matchTemp;
 	      prim.primerId = primerId;
 	      prim.genSeq = genomicseq;
               if (fwdrev == 0) {
                 prim.onFor = true;
-                prim.pos = chrpos;
+                prim.pos = chrpos + c.kmer - pSeq[primerId].size() - 1;
                 if (c.indel) addUnique(forBind[refIndex], prim, c.distance);
 		else forBind[refIndex].push_back(prim);
               } else {
                 prim.onFor = false;
-                prim.pos = chrpos + pSeq[primerId].size() - 1; 
+                prim.pos = chrpos + m - c.kmer + pSeq[primerId].size(); 
                 if (c.indel) addUnique(revBind[refIndex], prim, c.distance);
 		else revBind[refIndex].push_back(prim);
               }
